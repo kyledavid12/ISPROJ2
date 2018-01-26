d0088564b9a7e780335c7d15defd3193eb92824a
kyledavid12 <35282622+kyledavid12@users.noreply.github.com>
2018-01-26 18:10:06 +0800

    Add files via upload

--- viewstudentenrollee.php
+++ viewstudentenrollee.php
@@ -4,12 +4,30 @@
     validateAccess();
     validateAdmin();
 ?>
-		    <div class="sidebar" data-color="red">
-			<!--
-		        Tip 1: You can change the color of the sidebar using: data-color="purple | blue | green | orange | red"
+		 <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title> Sto. Nino Formation and Science School| Dashboard</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="../../../bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="../../../bower_components/font-awesome/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="../../../bower_components/Ionicons/css/ionicons.min.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="../../../bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../../../dist/css/AdminLTE.min.css">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="../../../dist/css/skins/skin-red.min.css">
 
-		        Tip 2: you can also add an image using data-image tag
-		    -->
+ 
+  <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
 
 			<div class="logo" style="background-color: #ffffff;">
 				<a href="<?php echo app_path ?>staff/index.php"><img src="<?php echo app_path ?>/dist/img/logo.png" oncontextmenu="return false" width="230" height="60" onmousedown="return false;"></a>
